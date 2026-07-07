72a1310041b0fe89be449d7752e7e93167b6fc59
Mohamed_Gabr <mohamedgbr20233@gmail.com>
2026-07-07 17:03:46 +0300

    Add input and output video files, and include Vite logo SVG
    - Added input.mp4 (9788 KB) and output.mp4 (4689 KB) to the videos directory.
    - Introduced vite.svg file containing the Vite logo.

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ import NavBar from './components/NavBar'
 gsap.registerPlugin(ScrollTrigger, SplitText)
 const App = () => {
   return (
-    <main>
+    <main className=''>
       <NavBar />
+      <div className=" bg-red-400 h-400 block w-full">s</div>
     </main>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,42 @@
+import { useGSAP } from "@gsap/react"
 import { navLinks } from "../../constants"
+import gsap from "gsap"
 
 const NavBar = () => {
+    // create glassy animation
+    useGSAP(() => {
+        // first define our triggered element how it will trigger and when 
+        const navTween = gsap.timeline({
+            scrollTrigger: {
+                trigger: "nav", //*=> trigger by the nav element 
+                start: "bottom top", //*=> bottom of the navbar touch the top of the viewport
+                end: "+=80",
+                scrub: true,
+                duration: 10
+            }
+        })
+
+        // then define our animation
+        navTween.fromTo(
+            "nav",
+            {
+                backgroundColor: "transparent",
+                backdropFilter: "blur(0px)",
+            },
+            {
+                backgroundColor: "#000000000050",
+                backdropFilter: "blur(10px)",
+                duration: 1,
+                ease: "power1.inOut",
+            }
+        );
+
+    }, [])
     return (
         <nav>
             <div>
                 <a href="#home" className="flex items-center gap-2">
+                    <img src="/images/logo.png" alt="" />
                     <p>Velvet Pour</p>
                 </a>
 
